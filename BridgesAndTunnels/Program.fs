f6eedfaee1573abfb7c279df002d128967a53ad5
emilj <emil.jessen94@gmail.com>
2020-09-26 00:40:08 +0200

    Bridges and tunnels only passes first two cases
    Need to implement
    DB MN -> 2
    SO SL -> 2
    current result is
    DB MN -> 2
    SO SL -> 4

---
+++ BridgesAndTunnels/Program.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let cases = Console.ReadLine() |> int
+    let rec createSet (acc:Set<string>) x n =
+        function
+            | "" | null -> x
+            | a when n>0 -> let buildings = a.Split(" ")
+                            let firstAcc = acc.Add(buildings.[0])
+                            let secondAcc = firstAcc.Add(buildings.[1])
+                            createSet (secondAcc) ((secondAcc.Count)::x) (n-1) (Console.ReadLine())
+            | _ -> x
+    let printlist = List.rev (createSet (Set.empty) [] cases (Console.ReadLine()))
+    let rec print =
+        function
+          | [] -> ()
+          | a::ac -> printfn "%d" a
+                     print ac
+    print printlist
+    0 // return an integer exit code
