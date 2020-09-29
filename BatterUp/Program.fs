d27c498db2c28120f20eba2a606b218f90a358ed
emilj <emil.jessen94@gmail.com>
2020-09-29 16:38:20 +0200

    Batter up is now accepted

--- BatterUp/Program.fs
+++ BatterUp/Program.fs
@@ -5,11 +5,14 @@ open System
 [<EntryPoint>]
 let main argv =
      let cases = Console.ReadLine() |> int
-     let numbers = Console.ReadLine().Split(" ") |> Array.toList
+     let numbers = Console.ReadLine().Split(" ") |> List.ofArray 
+     
      let rec createAvg acc n =
             function
                 | a::ac when n > 0 -> if a |> int >= 0 then createAvg ((a |> double)::acc) (n-1) ac
                                        else createAvg acc (n-1) ac
                 | _ -> List.average acc
      printfn "%f" (createAvg [] cases numbers)
+
+     
      0 // return an integer exit code
