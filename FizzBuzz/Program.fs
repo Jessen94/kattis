f3d3fffd316dec55609237b6e6406ccef839316a
emilj <emil.jessen94@gmail.com>
2020-10-03 16:06:55 +0200

    Got Nasty hacks and fizzbuzz accepted

---
+++ FizzBuzz/Program.fs
@@ -0,0 +1,25 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let numbers = Console.ReadLine().Split(" ")
+    let x = numbers.[0] |> int
+    let y = numbers.[1] |> int
+    let n = numbers.[2] |> int
+    let rec createList acc =
+        function
+            | 0 -> acc
+            | a -> match (a%x=0,a%y=0) with
+                        | (true,true) -> createList ("FizzBuzz"::acc) (a-1)
+                        | (true, false) -> createList ("Fizz"::acc) (a-1)
+                        | (false,true) -> createList ("Buzz"::acc) (a-1)
+                        | _ -> createList (a.ToString()::acc) (a-1)
+    let rec print =
+        function
+            | [] -> ()
+            | a::ac -> printfn "%s" a
+                       print ac
+    print (createList [] n)
+    0 // return an integer exit code
---
+++ NastyHacks/Program.fs
@@ -0,0 +1,26 @@
+﻿ // Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let cases = Console.ReadLine() |> int
+    let rec createPrintList acc =
+        function
+            | 0 -> acc
+            | a when a>0 ->
+                let numbers = Console.ReadLine().Split(" ")
+                let revenueForNotAd = (numbers.[0] |> int)
+                let revenueForAd = (numbers.[1] |> int) - (numbers.[2] |> int)
+                if revenueForAd = revenueForNotAd then createPrintList ("does not matter"::acc) (a-1)
+                elif revenueForAd > revenueForNotAd then createPrintList ("advertise"::acc) (a-1)
+                else createPrintList ("do not advertise"::acc) (a-1)
+            | _ -> acc
+    let rec print =
+        function
+            | [] -> ()
+            | a::ac -> printfn "%s" a
+                       print ac
+    print (List.rev (createPrintList [] cases))
+    
+    0 // return an integer exit code
